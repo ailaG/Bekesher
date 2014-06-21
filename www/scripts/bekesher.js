b5418493bc500b63e6ab57be6f55bb82d6c5bdf6
galia <smartgit@galiaba.com>
2014-06-21 04:35:59 +0300

    email-demo works (bug fixed)

--- www/scripts/bekesher.js
+++ www/scripts/bekesher.js
@@ -1,10 +1,19 @@
 
 
 var bekesher = angular.module('bekesher', [])
-
-	.controller('greetings', ['$scope', function($scope) {
-			$scope.greetings = [
-				{id: '1', title: 'Wordpress', text: 'I do Wordpress' },
-				{id: '2', title: 'Angular', text: 'My Angular is fab' }
-			];
-		}]);
+	.factory('greetingsF', function() {
+		return { greetings : [
+			{id: '1', title: 'Wordpress', text: 'I do Wordpress' },
+			{id: '2', title: 'Angular', text: 'My Angular is fab' },
+			{id: '3', title: 'FED', text: 'I\'m a front end developer' }
+		]};
+	})
+	.controller('greetings_form', function($scope, greetingsF) {
+		$scope.greetingsF = greetingsF;
+	})
+		
+	.controller('greetings_preview', function($scope, greetingsF) {
+		$scope.greetingsF = greetingsF;
+	})
+		
+;
